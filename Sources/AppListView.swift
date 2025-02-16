3b4fe1ee91274da29576a48efac278caa607c5d8
托尼老师五毛钱洗剪吹 <136478738@qq.com>
2025-02-16 17:00:48 +0800

    Update AppListView.swift

--- Sources/AppListView.swift
+++ Sources/AppListView.swift
@@ -43,11 +43,16 @@ struct AppItemView: View {
                 Text("复制路径后，打开“设置”，粘贴到搜索栏，再次选择全部，点击“共享”。\n\n仅支持iOS 18.2b1往下版本。对于这个漏洞，文件夹只能通过AirDrop共享。如果你正在分享App Store应用，请注意它仍将保持加密状态。")
             }
         }
-        .overlay(
-            ToastView(message: toastMessage, duration: 5, isVisible: $showToast)
-                .animation(.easeInOut, value: showToast)
-                .padding()
-        )
+       .overlay(
+            Group {
+                if showToast {
+                    ToastView(message: toastMessage, duration: 5, isVisible: $showToast)
+                        .animation(.easeInOut, value: showToast)
+                        .padding()
+      
+        }
+        }
+            )
         }
 }
 
