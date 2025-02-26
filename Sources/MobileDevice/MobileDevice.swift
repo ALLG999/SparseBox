5fd5923009d2612c2cfa093464a9969b670d7f2b
托尼老师五毛钱洗剪吹 <23072523+ALLG999@users.noreply.github.com>
2025-02-26 14:04:29 +0800

    Update MobileDevice.swift

--- Sources/MobileDevice/MobileDevice.swift
+++ Sources/MobileDevice/MobileDevice.swift
@@ -313,7 +313,7 @@ private func read_plist_to_binary_data(plist: plist_t?) -> Data? {
     defer { free(buf) }
     var len: UInt32 = 0
     guard plist_to_bin(plist, &buf, &len) == PLIST_ERR_SUCCESS,
-          let buf，
+          let buf,
           len > 0
     else { return nil }
     return Data(bytes: buf, count: Int(len))
