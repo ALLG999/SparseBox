b251a69ee2009c2e304fab12a1a0e39015f603be
托尼老师五毛钱洗剪吹 <23072523+ALLG999@users.noreply.github.com>
2025-02-26 10:52:52 +0800

    Update MyApp.swift

--- Sources/MyApp.swift
+++ Sources/MyApp.swift
@@ -1,18 +1,14 @@
 import SwiftUI
 
-// 应用主入口，使用@main标识
 @main
 struct MyApp: App {
     init() {
-        // 可选的初始化配置（示例中被注释）
-        // setenv("RUST_LOG", "trace", 1)  // 设置环境变量
-        // set_debug(true)                // 启用调试模式
+        //setenv("RUST_LOG", "trace", 1)
+        //set_debug(true)
     }
-    
-    // 定义应用主体结构
     var body: some Scene {
-        WindowGroup {          // 窗口组容器
-            ContentView()      // 主内容视图
+        WindowGroup {
+            ContentView()
         }
     }
 }
